ac89fc019ee5ea6a47479a11d7a6ca251e4071c5
Kevin W <gra230434@gmail.com>
2016-05-18 21:57:52 +0800

    2016 change for Comb laser lab

---
+++ index.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+
+get_header(); ?>
+
+<?php if ( is_home() ): ?>
+	<div id="comblaser_about_us" class="comblaseraboutus">
+		<div class="CAU-text site-inner">
+			<div class="CAU-title">
+	  	  <h2>ABOUT US</h2>
+	  	  <p>關於我們</p>
+			</div>
+			<div class="CAU-post CAU-half CAU-half-title">
+				<h3>test test test test twentysixteen the custom logo</h3>
+			</div>
+			<div class="CAU-post CAU-half">
+				<p>
+					But when it comes to natural beauty in Japan, it's hard to top Tohoku.Located at the tip of Japan's main island of Honshu, the Tohoku region is made up of six prefectures: Aomori, Akita, Fukushima, Iwate, Miyagi and Yamagata.
+				</p>
+        <p>
+					Though devastated by the tsunami and earthquake in 2011, Tohoku's recovery has been swift.Japanese and foreign visitors alike are returning to the area -- and visiting in greater numbers -- for its timeless art, culture, food and scenery that's truly breathtaking in every season.
+				</p>
+				<p>
+					We've put together a gallery of natural Tohoku highlights that offers a glimpse of the beauty this region has to offer.
+				</p>
+      </div>
+			<div class="clear"></div>
+	  </div>
+	</div>
+	<div id="comblaser_our_team" class="comblaserourteam">
+		<div class="COT-text site-inner">
+		  <h2>OUR TEAM</h2>
+		  <p>我們團隊</p>
+		</div>
+	</div>
+<?php endif ?>
+<!-- 12345678 -->
+<div class="site-inner">
+  <div id="content" class="site-content">
+	  <div id="primary" class="content-area">
+		  <main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php if ( is_home() && ! is_front_page() ) : ?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+			<?php endif; ?>
+
+			<?php
+			// Start the loop.
+			while ( have_posts() ) : the_post();
+
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content', get_post_format() );
+
+			// End the loop.
+			endwhile;
+
+			// Previous/next page navigation.
+			the_posts_pagination( array(
+				'prev_text'          => __( 'Previous page', 'twentysixteen' ),
+				'next_text'          => __( 'Next page', 'twentysixteen' ),
+				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>',
+			) );
+
+		// If no content, include the "No posts found" template.
+		else :
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
+		?>
+
+		  </main><!-- .site-main -->
+	</div><!-- .content-area -->
+
+<?php if( ! is_home() ):
+	get_sidebar();
+endif; ?>>
+
+<?php get_footer(); ?>
