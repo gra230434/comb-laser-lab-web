c38462aa4e234cf3d601e0d7e5cfee993fc747ec
Kevin W <gra230434@gmail.com>
2016-07-04 10:30:39 +0800

    home page change

--- index.php
+++ index.php
@@ -25,16 +25,12 @@ get_header(); ?>
 	  	  <h4>關於我們</h4>
 			</div>
 			<div style="margin-bottom:50px;">
-				<img src="<?php bloginfo('template_directory'); ?>/image/about.jpg">
+				<img style="margin-top:30px;" src="<?php bloginfo('template_directory'); ?>/image/about.jpg">
 			</div>
 			<div class="CAU-post CAU-half-left">
-				<div class="CAU-half-title">
-				  <h3>大家好，我是實驗室負責人鄭王曜</h3>
-					<p>主要負責籌錢，讓大家工作開心，還有負責解決同學們實驗上的問題，包含實驗的阻礙與心靈的創傷，求診完的同學通常含笑離開，因為我十個建議常有一個會是對的。</p>
-					<p>其實我也會懷疑，那一個對的是因為同學問診時描述錯誤所致．但基本上，我在實驗室，對學生而言還是有用的，此點必然無誤。</p>
-					<span><strong>TEL:</strong>886-3-4227151 ext 65337</span><br>
-					<span><strong>Email:</strong>wycheng@ncu.edu.tw</span>
-				</div><!-- CAU-half-title -->
+				<div style="float:right;width:90%;margin-right:10px;">
+				<img style="border: 1px solid #ddd;border-radius: 4px;padding: 5px;" src="<?php bloginfo('template_directory'); ?>/image/about_1.jpg">
+			</div>
 			</div>
 			<div class="CAU-post CAU-half-right">
 				<div class="CAU-half-post">
@@ -95,11 +91,17 @@ get_header(); ?>
 
 			  <?php // Start the loop. ?>
 				<ul class='home-post'>
-					<?php while ( have_posts() ) : the_post();
-					echo "<li class='home-post-li'>";
-					get_template_part( 'template-parts/content', "home" );
-					echo "</li>";
+					<?php
+          $postnumber = 1;
+					while ( have_posts() & $postnumber < 5 ) : the_post();
+					echo "<li class='home-post-li'>";?>
+						<article id="post-<?php the_ID(); ?>" class="home-content home-content-<?php echo $postnumber; ?>">
+					<?php
+						get_template_part( 'template-parts/content', "home" );
+						echo "</li></article><!-- #post-## -->";
+						$postnumber = $postnumber + 1;
 		  		endwhile;// End the loop.?>
+					<div class="clear"></div>
 				</ul><!-- .home-post -->
 				<?php // home post end ?>
 
@@ -107,14 +109,16 @@ get_header(); ?>
 			  <main id="main" class="site-main" role="main">
 			  <?php // Start the loop.
 			    while ( have_posts() ) : the_post();
-			      /*
+
+					  /*
 			       * Include the Post-Format-specific template for the content.
 			       * If you want to override this in a child theme, then include a file
 			       * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 			       */
 			      get_template_part( 'template-parts/content', get_post_format() );
+
 			    // End the loop.
-			    endwhile;
+				endwhile;
 
 			    // Previous/next page navigation.
 			    the_posts_pagination( array(
--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -8,7 +8,7 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" class="home-content">
+
 	<header class="home-entry-header">
 
 		<?php the_title( sprintf( '<h2 id="home-post-title" class="home-post-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
@@ -35,4 +35,3 @@
 			edit_post_link(__( 'Edit', 'twentysixteen' ),'<span class="edit-link">','</span>');
 		?>
 	</footer><!-- .entry-footer -->
-</article><!-- #post-## -->
